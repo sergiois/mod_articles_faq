59a6e3b274cf7d6cbdd1769fcdf8a6a9554ec6c9
Tazzios <23451105+Tazzios@users.noreply.github.com>
2020-04-16 16:28:11 +0200

    Update default_bootstrap3.php
    add modal box option

--- tmpl/default_bootstrap3.php
+++ tmpl/default_bootstrap3.php
@@ -122,13 +122,17 @@ $jinput = Factory::getApplication()->input;
                 
               <?php if($params->get('show_readmore')): ?>
                     <p class="text-right"> 
-                        <?php if($params->get('readmore_new_window')== false): ?>
+                        <?php if($params->get('readmore_behaviour')== 0): ?>
                     
-                        <a href="<?php echo $item->link; ?>" class="btn btn-primary"><?php echo $params->get('readmore_text') ? $params->get('readmore_text') : Text::_('MOD_ARTICLES_FAQ_FIELD_READMORE_TEXT'); ?></a>
+                        <a href="<?php echo $item->link; ?>" class="btn btn-primary" ><?php echo $params->get('readmore_text') ? $params->get('readmore_text') : Text::_('MOD_ARTICLES_FAQ_FIELD_READMORE_TEXT'); ?></a>
                     
-                        <?php else: ?>
+                        <?php elseif($params->get('readmore_behaviour')== 1): ?>
                         <a href="<?php echo $item->link; ?>" class="btn btn-primary"  target="_blank"><?php echo $params->get('readmore_text') ? $params->get('readmore_text') : Text::_('MOD_ARTICLES_FAQ_FIELD_READMORE_TEXT'); ?></a>
-                    
+
+                        <?php else: ?>
+			    <?php JHTML::_('behavior.modal'); ?>
+                        <a href="<?php echo $item->link; ?>?tmpl=component" class="btn btn-primary modal"  class="modal" rel="{handler: 'iframe', size: {x: <?php echo $params->get('readmore_modal_width') ?>, y: <?php echo $params->get('readmore_modal_height')?>}}"><?php echo $params->get('readmore_text') ? $params->get('readmore_text') : Text::_('MOD_ARTICLES_FAQ_FIELD_READMORE_TEXT'); ?></a>
+                            
                         <?php endif; ?>
                     </p> 
                 <?php endif; ?>
